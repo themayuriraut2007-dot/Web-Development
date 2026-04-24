8af90c2a06c01aef2379b4a989ae564602d22bd3
mayuri raut <themayuriraut2007>
2026-04-23 20:41:25 -1200

    add new change

--- javascript/index1.js
+++ javascript/index1.js
@@ -1,3 +1,34 @@
-console.log("hi")
-window.alert("try again")
-document.write("well done")
+ console.log("hi")
+ window.alert("try again")
+ document.write("well done")
+ 
+
+let n1=40
+let n2=30
+
+let sum =40+30
+let sub =40-30
+let multi =40*30
+let div =40/30
+
+console.log(sum)  
+console.log(sub)
+console.log(multi)
+console.log(div)
+
+let data = 200
+let result=typeof(data)
+console.log(typeof(data))
+window.prompt()
+
+// let data =window.prompt("enter your message")
+// console.log(data)
+// console.log(typeof(data)) 
+
+// TYPE CASTING 
+
+let fname = "mayuri"
+let mname = "prabhakar"
+let lname = "raut"
+
+console.log(fname + mname + lname)
