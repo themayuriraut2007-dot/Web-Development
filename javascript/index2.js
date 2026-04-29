9111ff65c1f1423d2ea41668387bf2f51033e732
mayuri raut <themayuriraut2007>
2026-04-28 19:02:50 -1200

    Intial commit

---
+++ javascript/index2.js
@@ -0,0 +1,114 @@
+// Increment and Decrement(++ , -- )
+
+//  let data = 70
+
+//  let result = 70 + --data
+
+//  console.log(result)
+
+//  console.log(data--) 
+//  console.log(data) 
+
+
+
+
+//  ( type casting )
+
+
+//  number -> string
+//  number -> boolean
+
+//  string -> number
+//  string -> boolean 
+
+//  boolean -> number
+//  boolean -> string
+
+   // Example -->
+
+//  let data = 567
+
+// console.log(typeof (data))
+// console.log(data)
+
+//  number -> string
+
+// let data= 1122
+
+//  data = String(data)
+//  data = data.toString()
+
+// console.log(typeof (data))
+// console.log(data)
+
+
+//  let data = 1234
+
+// console.log(typeof (data))
+// console.log(data)
+
+
+//  number -> boolean(true/false)
+
+//  data = Boolean(data)
+//  (all value expect 0 is true for boolean)
+
+//  console.log(typeof (data))
+//  console.log(data)
+
+//  let data = "12.45271"
+
+//  console.log(typeof (data))
+//  console.log(data)
+
+// string -> number
+
+//  console.log(typeof (data))
+//  console.log(data)
+
+//  let data = ""
+
+// console.log(typeof (data))
+// console.log(data)
+
+//  string -> boolean
+    
+//  let data = ""  
+//  data = Boolean(data)
+
+
+//  empty string is false else true
+
+// console.log(typeof (data))
+//  console.log(data)
+
+
+//  let data = false
+
+// console.log(typeof (data))
+//  console.log(data)
+
+
+// boolean -> number
+
+ let data = false
+ data = Number(data)
+
+// if true then 1
+//  if false then 0 
+
+console.log(typeof (data))
+console.log(data)
+
+
+//  boolean -> string
+
+//  let data = false
+//  data = String(data)
+
+//  if true then true
+//  if false then false
+
+// console.log(typeof (data))
+// console.log(data)
+
