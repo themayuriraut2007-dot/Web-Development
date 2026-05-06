7f4fb078641c84253272e75d66fb830a7ee634fb
mayuri raut <themayuriraut2007>
2026-05-05 21:02:13 -1200

    new file

---
+++ javascript/loop.js
@@ -0,0 +1,86 @@
+// While Loop -->
+
+
+// Factorial
+
+// let number = 5
+// let fact = 1
+
+// while (number != 0){
+//     fact = fact * number //5 , 4//20 , 3//60,2//120,1//120,0
+//     number--
+// }
+
+// console.log(fact)
+
+
+//  let count = 0;
+
+// while (count<=10) {
+//     console.log(count+count)
+//     count++
+// }
+
+
+// Table Create 
+
+// let i = 1;
+// let num = 3
+// while(i<=10) {
+//    console.log(i*num)
+//    i++
+// }
+
+
+// Square 
+
+// let i = 3;
+// while(i<=10) {
+//    console.log(i*i)
+//    i++
+// }
+
+
+
+
+// Do While Loop -->
+
+
+//Even number 
+
+// let i= 1;
+//  do {
+//    if(i%2==0)
+//    console.log(i)
+//    i++
+//  }
+//  while(i<=40)
+
+
+
+// do {
+
+//     let year = Number(window.prompt("enter year !"))
+
+//     if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+//         console.log('year leap !')
+//     } else {
+//         console.log("not a leap year !")
+//     }
+
+//     choice = confirm("do you wish to continue !")
+
+// }while(choice)
+
+
+
+// For Loop -->
+
+let i = 1;
+for(i=1;i<=40;i++)
+{
+   if(i%2!=0)
+   console.log(i)
+}
+
+
