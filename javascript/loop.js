7d0bcbca0f37259f5080d2137fc4aa2fa3cd08dc
mayuri raut <themayuriraut2007>
2026-05-06 20:44:16 -1200

    new change

--- javascript/loop.js
+++ javascript/loop.js
@@ -76,11 +76,70 @@
 
 // For Loop -->
 
-let i = 1;
-for(i=1;i<=40;i++)
-{
-   if(i%2!=0)
-   console.log(i)
+// let i = 1;
+// for(i=1;i<=40;i++)
+// {
+//    if(i%2!=0)
+//    console.log(i)
+// }
+
+
+//Sum of Digits
+
+// let sum=0;
+// let number=1234
+
+// while(number!=0) {
+//    d=number%10;
+//    sum=sum+d;
+//    number=parseInt(number/10);
+// }
+// console.log(sum)
+
+
+
+// Using for loop 
+
+// let sum=0;
+
+// for(number= 1;number<=20;number++)
+// {
+//    console.log(number)
+//    sum=sum+number
+
+// }
+// console.log("total sum : ",sum)
+
+
+
+// reverse
+
+// let rev=0;
+// let number=1234
+
+// while(number!=0) {
+//    d=number%10;
+//    rev=rev*10+d
+//    number=parseInt(number/10);
+// }
+// console.log(rev)
+
+
+
+// Using for loop
+
+// let rev=0;
+// for(let number=1;number<=5;number++)
+// {
+//    console.log(number)
+//    rev=rev*10+number;
+// }
+// console.log(rev)
+
+
+let result = " ";
+for (let i=5;i>=1;i--) {
+     result = result +i;
 }
-
-
+console.log(result)
+   
