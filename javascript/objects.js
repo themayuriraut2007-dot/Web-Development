0a9d870fb68b0e29729c6f368347b45a9860a5cb
mayuri raut <themayuriraut2007>
2026-05-17 19:58:50 -1200

    new file

---
+++ javascript/objects.js
@@ -0,0 +1,60 @@
+// let myData = {
+//     name: "Mayuri",
+//     age: 20,
+//     height: 165.59 ,
+//     weight: 46,
+//     address: "new nandanvan nagpur",
+//     phone: 8957343485
+// }
+
+// console.log(myData.name.slice(myData.name.indexOf(" "), myData.name.lastIndexOf(" ")))  
+
+// console.log(myData.name.slice(0,myData.name.indexOf(" "))) 
+
+// console.log(myData.name.slice(myData.name.lastIndexOf(" "), myData.name.length)) 
+
+// console.log(myData.height)
+// console.log(myData.weight)
+// console.log(myData.address)
+// console.log(myData.phone)
+
+// console.log(myData)
+
+
+let myData = {
+    name: "Mayuri",
+    age: 23,
+    height: 164.59 ,
+    weight: 48,
+    address: "nagpur",
+    phone: 8957343485 ,
+
+    education: {
+
+        SSC: {
+            school : "Jagrut Vidyalaya Warud",
+            percentage : "89.53",
+            passingyear : 2021
+   
+        } ,
+         
+        HCS : {
+            school : "Jagrut Vidyalaya Warud",
+            percentage : "72.64",
+            passingyear : 2023
+   
+        },
+
+        Graduation : {
+            college : "Priyadarshini College Of Engineering Nagpur",
+            Degree : "B Tech (Computer Science)",
+        }
+        
+
+    }
+        
+       
+} 
+
+console.log(myData.education)
+console.log(myData)
