f713be0f60b500679d4cf50073fb64ccf0b66780
mayuri raut <themayuriraut2007>
2026-05-24 19:36:36 -1200

    new change

--- javascript/function.js
+++ javascript/function.js
@@ -30,3 +30,93 @@ factorial(5)
 factorial(4)
 factorial(2)
 factorial(9)
+
+
+
+ // 4 types of function
+
+// no args and no return
+
+// function sayHello() {
+//     console.log('hello')
+// }
+
+// sayHello()
+// sayHello()
+// sayHello()
+
+
+//  args and return 
+
+// function add(n1, n2) {
+//     let sum = n1 + n2
+//     return sum
+// }
+
+// let result = add(10, 20) //30
+
+// console.log(result)
+
+// console.log(add(10, 21)) //31
+
+// if (add(20, 22) < 35) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// // args but no return 
+
+// function factorial(number) {
+//     let org_number = number
+//     let fact = 1;
+//     while (number != 0) {
+//         fact = fact * number
+//         number--
+//     }
+//     console.log(`factorial of ${org_number} is ${fact}`)
+// }
+
+// factorial(5)
+
+// // no args but return 
+
+// // message function
+
+// function isError() {
+//     return true
+//     // console.log(1)
+//     // console.log(2)
+//     // console.log(3)
+// }
+
+// function isSuccess() {
+//     return true
+// }
+
+let numbers = [1, 2, 3, 4, 5]
+
+numbers.forEach(function (element, index) {
+    console.log(element, " at index ", index)
+})
+
+// let newArray = numbers.forEach((element, index) => {
+//     if (element % 2 == 0) {
+//         return element
+//     }
+// })
+
+let newArray = numbers.map((element, index) => {
+    if (element % 2 == 0) {
+        return element
+    }
+})
+
+let newArray1 = numbers.filter((element) => {
+    return element % 2 == 0
+})
+
+console.log(newArray)
+console.log(newArray1)
+
+// forEach cannot return anything
