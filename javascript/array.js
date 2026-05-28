309883bad802c5c54b6fc6bd165721bbc769dec4
mayuri raut <themayuriraut2007>
2026-05-27 18:53:06 -1200

    new changes

--- javascript/array.js
+++ javascript/array.js
@@ -52,7 +52,7 @@
 
 
 
-let numbers = [1, 2, 3, 4, 5]
+// let numbers = [1, 2, 3, 4, 5]
 
 // // console.log(numbers.length) // 5
 // // // element add, remove 
@@ -125,22 +125,165 @@ let numbers = [1, 2, 3, 4, 5]
 // // looping for array [for of, for each, map, filter]
 
 
-let fruits = "apple,mango,banana,dargon fruit,grapes,water melon"
-let fruitArray = fruits.split(",")
+// let fruits = "apple,mango,banana,dargon fruit,grapes,water melon"
+// let fruitArray = fruits.split(",")
 
-// for of
+// // for of
 
-for(let x of fruitArray){
-    console.log("i like "+x)
+// for(let x of fruitArray){
+//     console.log("i like "+x)
+// }
+
+// //for each 
+
+// const sqNumber = (x) => {
+//     console.log(x*x)
+// }
+
+// numbers.forEach(sqNumber)
+
+// let newNumbers = [5,6,7,8,9,10]
+// newNumbers.forEach(sqNumber)
+
+
+
+
+// linear search -->
+
+//  let numbers = [10, 12, 3, 15, 6, 2, 1, 5, 9]
+
+//  let searchValue = 5
+
+// let position = 0
+
+//  let flag = false
+
+//  let steps = 0
+
+// for (let i = 0; i < numbers.length; i++) {
+//     steps++
+//     if (numbers[i] == searchValue) {
+//         position = i + 1
+//         flag = true
+//         break;
+//     }
+// }
+
+// if (flag) {
+//     console.log('we found element ${searchValue} at position ${position} !')
+//     console.log('in ${steps} steps')
+// } else {
+//     console.log('unable to found element ${searchValue} in array !')
+//     console.log('in ${steps} steps')
+// }
+
+// time complexity -> O(n)
+
+
+
+// binary search -->
+
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15]
+
+// let searchValue = 2
+
+// let flag = false
+
+// let lb = 0
+
+// let ub = numbers.length - 1
+
+// let position = 0
+
+// let steps = 0
+
+// let midPoint;
+// while (lb < ub) {
+//     steps++
+
+//     midPoint = Math.ceil((lb + ub) / 2)//rounding off to get intergers as the index cannot be float points
+
+//     if (numbers[midPoint] == searchValue) {
+//         flag = true
+//         position = midPoint + 1
+//         break;
+//     }
+
+//     if (numbers[midPoint] < searchValue) {
+//         lb = midPoint + 1
+//         // RHS
+//     } else {
+//         ub = midPoint - 1
+//     }
+// }
+
+// if (flag) {
+//     console.log('we found element ${searchValue} at position ${position} !')
+//     console.log('in ${steps} steps')
+// } else {
+//     console.log('unable to found element ${searchValue} in array !')
+//     console.log('in ${steps} steps')
+// }
+
+
+
+
+
+// bubble sort -->
+
+
+// let numbers = [5, 5, 2, 1, 2, 8, 0, -9]
+
+// let swaped = false
+
+// for (let i = 0; i < numbers.length - 1; i++) {
+
+//     swaped = false
+
+//     for (let j = 0; j < numbers.length - i - 1; j++) {
+//         if (numbers[j] > numbers[j + 1]) {
+//             // swap
+//             let temp = numbers[j]
+//             numbers[j] = numbers[j + 1]
+//             numbers[j + 1] = temp
+
+//             swaped = true
+
+//         }
+//     }
+//     if (!swaped) break
+// }
+
+// console.log("sorted array")
+// console.log(numbers)
+
+
+
+// selection sort -->
+
+
+let numbers = [3, 5, 2, 1, 4, 0, -1, 5, 2]
+
+for (let i = 0; i < numbers.length - 1; i++) {
+    minIndex = i
+    // swaping 
+    for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[j] < numbers[minIndex]) {
+            minIndex = j
+        }
+    }
+    // swap
+    let temp = numbers[minIndex]
+    numbers[minIndex] = numbers[i]
+    numbers[i] = temp
 }
 
-//for each 
+console.log("sorted array : ")
+console.log(numbers)
 
-const sqNumber = (x) => {
-    console.log(x*x)
-}
+// following is a direct to print the sorted array -->
 
-numbers.forEach(sqNumber)
+// numbers.sort((a, b) => b - a)
 
-let newNumbers = [5,6,7,8,9,10]
-newNumbers.forEach(sqNumber)
+// console.log(numbers)
