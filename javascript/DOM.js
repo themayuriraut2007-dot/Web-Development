d5be93fb3c1d77f886684f82a7d2376b4b4e4fe5
mayuri raut <themayuriraut2007>
2026-06-07 22:09:19 -1200

    new changes

--- javascript/DOM.js
+++ javascript/DOM.js
@@ -1,4 +1,4 @@
-console.log("hello World")
+// console.log("hello World")
 
 // document.getElementById("redbox").style.backgroundColor = "red"
 
@@ -14,11 +14,11 @@ console.log("hello World")
 
 // Agar pure karne hai to use this querySelectorAll(multiple element return krta hai)
 
-console.log(document.querySelectorAll(".box"))
+// console.log(document.querySelectorAll(".box"))
 
-document.querySelectorAll(".box").forEach(e =>{
-   e.style.backgroundColor = "green"
-})
+// document.querySelectorAll(".box").forEach(e =>{
+//    e.style.backgroundColor = "green"
+// })
 
 
 
@@ -26,4 +26,52 @@ document.querySelectorAll(".box").forEach(e =>{
 // document.getElementsByTagName("div")
 //  it returns html collection
 
+
+
+
+
+// let target = document.getElementsByTagName("h2")
+// let target = document.getElementsByClassName("fourth")
+// let target = document.getElementById("i3")
+
+
+// let targets = document.querySelectorAll("li:nth-child(2)")
+
+// console.log(target)
+// console.log(targets)
+
+
+
+
+// let students = [1,2]
+
+// if (students.length != 0) {
+//     console.log('students exits !')
+// } else {
+//     console.log("they don't")
+// }
+
+// let target = document.getElementById("btn1")
+
+// console.log(target)
+
+
+
+
+
+// toggle image
+
+// let image = document.getElementById('img1')
+// let btn = document.getElementById('btn1')
+
+// let show = true
+
+// btn.onclick = () => {
+//     show = !show
+//     if (show) {
+//         image.style.display = "block"
+//     } else {
+//         image.style.display = "none"
+//     }
+// }
 
